d1f3295cf6a088fd2a35dbfc63a759df40f5d963
Usman Ajupov <au073@mail.ru>
2017-09-23 02:17:33 +0300

    Маппер

--- Aklion.Crm/Controllers/StoresController.cs
+++ Aklion.Crm/Controllers/StoresController.cs
@@ -5,6 +5,7 @@ using Aklion.Crm.Dao.Store.Models;
 using Aklion.Crm.Models;
 using Aklion.Crm.Models.Store;
 using Aklion.Infrastructure.Storage.Repository;
+using Aklion.Infrastructure.Utils.Mapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aklion.Crm.Controllers
@@ -29,18 +30,8 @@ namespace Aklion.Crm.Controllers
         {
             var result = await _repository.Get<Store>(model).ConfigureAwait(false);
 
-            var list = result.Item2.Select(x => new StoreGetResponseModel
-            {
-                Id = x.Id,
-                CreateUserId = x.CreateUserId,
-                Name = x.Name,
-                ApiKey = x.ApiKey,
-                ApiSecret = x.ApiSecret,
-                IsLocked = x.IsLocked,
-                IsDeleted = x.IsDeleted,
-                CreateDate = x.CreateDate,
-                ModifyDate = x.ModifyDate
-            }).ToList();
+            //TODO Написать маппер для списка моделей
+            var list = result.Item2.Select(x => x.MapNew<Store, StoreGetResponseModel>()).ToList();
 
             return new BaseGetListResponseModel<StoreGetResponseModel>(list, result.Item1, model.Page, model.Size);
         }
@@ -48,37 +39,18 @@ namespace Aklion.Crm.Controllers
         [HttpGet]
         public async Task<StoreGetResponseModel> Get(int id)
         {
-            var result = await _repository.Get<Store>(id).ConfigureAwait(false);
+            var model = await _repository.Get<Store>(id).ConfigureAwait(false);
 
-            return new StoreGetResponseModel
-            {
-                Id = result.Id,
-                CreateUserId = result.CreateUserId,
-                Name = result.Name,
-                ApiKey = result.ApiKey,
-                ApiSecret = result.ApiSecret,
-                IsLocked = result.IsLocked,
-                IsDeleted = result.IsDeleted,
-                CreateDate = result.CreateDate,
-                ModifyDate = result.ModifyDate
-            };
+            return model.MapNew<Store, StoreGetResponseModel>();
         }
 
         [HttpPost]
         public async Task Create(StoreCreateRequestRequestModel model)
         {
-            var store = new Store
-            {
-                CreateUserId = model.CreateUserId,
-                Name = model.Name,
-                ApiKey = model.ApiKey,
-                ApiSecret = model.ApiSecret,
-                IsLocked = model.IsLocked,
-                IsDeleted = model.IsDeleted,
-                CreateDate = DateTime.Now
-            };
+            var newModel = model.MapNew<StoreCreateRequestRequestModel, Store>();
+            newModel.CreateDate = DateTime.Now;
 
-            await _repository.Create(store).ConfigureAwait(false);
+            await _repository.Create(newModel).ConfigureAwait(false);
         }
 
         [HttpPost]
@@ -86,12 +58,7 @@ namespace Aklion.Crm.Controllers
         {
             var store = await _repository.Get<Store>(model.Id).ConfigureAwait(false);
 
-            store.CreateUserId = model.CreateUserId;
-            store.Name = model.Name;
-            store.ApiKey = model.ApiKey;
-            store.ApiSecret = model.ApiSecret;
-            store.IsLocked = model.IsLocked;
-            store.IsDeleted = model.IsDeleted;
+            model.Map(store);
             store.ModifyDate = DateTime.Now;
 
             await _repository.Update(store).ConfigureAwait(false);
