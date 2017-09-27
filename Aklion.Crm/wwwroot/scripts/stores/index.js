343e05d1829b945a01bb91b0c848fd6649f9cfa8
Usman Ajupov <au073@mail.ru>
2017-09-28 01:31:24 +0300

    Прродолжаем

--- Aklion.Crm/wwwroot/scripts/stores/index.js
+++ Aklion.Crm/wwwroot/scripts/stores/index.js
@@ -21,14 +21,14 @@ function createHead(tableWrapper, columns, dataUrl) {
     $.each(columns,
         (i, e) => {
             // Create cell
-            html += '<th ' + (e.hidden ? 'hidden ' : '') + 'width="' + e.width + '">';
+            html += '<th class="table-sorting-cell"' + (e.hidden ? 'hidden ' : '') + 'width="' + e.width + '">';
 
             // Create sorting button
             if (e.sorting) {
-                html += '<span class="pointer ' + tableId + '-table-sorting" data-label="' + e.label + '"' +
+                html += '<button class="' + tableId + '-table-sorting" data-label="' + e.label + '"' +
                     'data-sorting-order="asc" data-sorting-column="' + e.name + '">';
                 html += e.label;
-                html += '</span>';
+                html += '</button>';
             } else {
                 html += '<span>';
                 html += e.label;
@@ -49,11 +49,11 @@ function createHead(tableWrapper, columns, dataUrl) {
             // Create filter input
             if (e.search) {
                 if (e.type === 'int' || e.type === 'string') {
-                    html += '<input type="text" class="' + tableId + '-table-filter" />';
+                    html += '<input type="text" class="' + tableId + '-table-filter" data-filter-name="' + e.name + '"/>';
                 } else if (e.type === 'bool') {
-                    html += '<select>';
+                    html += '<select data-filter-name="' + e.name + '">';
                     html += '<option>';
-                    html += 'Неизвестно';
+                    html += '';
                     html += '</option>';
                     html += '<option>';
                     html += 'Да';
@@ -63,7 +63,7 @@ function createHead(tableWrapper, columns, dataUrl) {
                     html += '</option>';
                     html += '</select>';
                 } else if (e.type === 'datetime') {
-                    html += '<input type="text" class="datepicker ' + tableId + '-table-filter" />';
+                    html += '<input type="text" class="datepicker ' + tableId + '-table-filter" data-filter-name="' + e.name + '"/>';
                 }
             }
 
@@ -105,17 +105,32 @@ function createHead(tableWrapper, columns, dataUrl) {
         // Set current sorting order
         if (sortingOrder === 'desc') {
             sortingButton.attr('data-sorting-order', 'asc');
-            sortingButton.html('<i class="fa fa-sort-down"></i>' + sortingButtonLabel);
+            sortingButton.html(sortingButtonLabel + '<i class="fa fa-sort-down"></i>');
         } else {
             sortingButton.attr('data-sorting-order', 'desc');
-            sortingButton.html(' <i class="fa fa-sort-up"></i>' + sortingButtonLabel);
+            sortingButton.html(sortingButtonLabel + '<i class="fa fa-sort-up"></i>');
         }
 
-        createBody(tableWrapper, columns, dataUrl, sortingColumn, sortingOrder);
+        // Get filters
+        var filters = {};
+
+        $('.' + tableId + '-table-filter').each((ci, ce) => {
+            var column = $(ce);
+            var name = column.attr('data-filter-name');
+            var value = column.val();
+
+            if (name.length > 0 && value.length > 0) {
+                filters[name] = value;
+            }
+        });
+
+
+
+        createBody(tableWrapper, columns, dataUrl, 1, 10, sortingColumn, sortingOrder, filters);
     });
 }
 
-function createBody(tableWrapper, columns, dataUrl, page, size, sortingColumn, sortingOrder) {
+function createBody(tableWrapper, columns, dataUrl, page, size, sortingColumn, sortingOrder, filters) {
     var tableId = tableWrapper[0].id;
     var table = $('table#' + tableId + '-table');
 
@@ -131,6 +146,8 @@ function createBody(tableWrapper, columns, dataUrl, page, size, sortingColumn, s
         SortingOrder: sortingOrder
     };
 
+    data = Object.assign(data, filters);
+
     // Get rows
     $.get(dataUrl,
         data,
@@ -139,44 +156,70 @@ function createBody(tableWrapper, columns, dataUrl, page, size, sortingColumn, s
                 return;
             }
 
-            var notHiddenColumnsCount = 0;
-            var isFirstRow = true;
-            var html = '<tbody>';
+            var html = '';
 
+            // Not hidden column count for calculatning footer width
+            var notHiddenColumnsCount = 0;
+
+            // Calculate by first row
+            var isFirstRow = true;
+
+            // Create body
+            html += '<tbody>';
             $.each(x.Items,
                 (ii, ie) => {
                     html += '<tr>';
 
                     $.each(columns,
                         (ci, ce) => {
+                            // If item has property with column name
                             if (ie.hasOwnProperty(ce.name)) {
 
+                                // Count increment if not hidden column
                                 if (isFirstRow && !ce.hidden) {
                                     notHiddenColumnsCount++;
                                 }
 
+                                // Item value
                                 var value = ie[ce.name];
 
                                 html += '<td ' + (ce.key ? ' data-id="' + value + '"' : '') +
-                                    ' align="' + ce.align + '"' + (ce.hidden ? ' hidden' : '') + '>';
+                                    ' style="text-align: ' + ce.align + '"' + (ce.hidden ? ' hidden' : '') + '>';
 
-                                if (ce.type === 'int' || ce.type === 'string') {
+                                // Format as is
+                                if (ce.type === 'int' || ce.type === 'decimal' ||
+                                    ce.type === 'double' || ce.type === 'string') {
                                     if (value !== null) {
                                         html += value;
                                     }
                                 }
 
+                                // Format as string date
+                                if (ce.type === 'date') {
+                                    if (value !== null) {
+                                        var date = new Date(value);
+
+                                        html += date.toLocaleDateString('ru-RU');
+                                    }
+                                }
+
+                                // Format as string date time
                                 if (ce.type === 'datetime') {
                                     if (value !== null) {
                                         var dateTime = new Date(value);
+
                                         html += dateTime.toLocaleDateString('ru-RU') + ' в ' +
                                             dateTime.toLocaleTimeString('en-GB');
                                     } 
                                 }
 
+                                // Format as yes/no
                                 if (ce.type === 'bool') {
-                                    var checked = value ? 'checked="checked"' : '';
-                                    html += '<input type="checkbox" disabled ' + checked +'></input>';
+                                    if (value) {
+                                        html += 'Да';
+                                    } else {
+                                        html += 'Нет';
+                                    }
                                 }
 
                                 html += '</td>';
@@ -188,55 +231,77 @@ function createBody(tableWrapper, columns, dataUrl, page, size, sortingColumn, s
                 });
             html += '</tbody>';
 
+            // Append body
             table.append(html);
+            html = '';
 
-            /////Foofter
-            var tfooter = '';
-            tfooter += '<tfoot>';
-            tfooter += '<tr>';
-            tfooter += '<td colspan="' + notHiddenColumnsCount + '">';
+            // Create footer
+            html += '<tfoot>';
+            html += '<tr>';
+            html += '<td colspan="' + notHiddenColumnsCount + '">';
 
-            tfooter += '<input type="button" class="table-button" value="◄◄">';
-            tfooter += '<input type="button" class="table-button" value="◄">';
-            tfooter += 'Стр.';
-            tfooter += '<input type="text"  class="table-text" value="' + x.Page + '">';
-            tfooter += ' из ' + x.PageCount;
-            tfooter += '<input type="button" class="table-button" value="►">';
-            tfooter += '<input type="button" class="table-button" value="►►">';
+            html += '<div>';
+            html += '<button>';
+            html += '<i class="fa fa-fast-backward"></i>';
+            html += '</button>';
 
+            html += '<button>';
+            html += '<i class="fa fa-backward"></i>';
+            html += '</button>';
 
+            html += '<div>';
+            html += '<span>';
+            html += 'Стр. ';
+            html += '</span>';
+
+            html += '<input type="text" value="' + x.Page + '">';
+
+            html += '<span>';
+            html += ' из ' + x.PageCount;
+            html += '</span>';
+            html += '</div>';
+
+            html += '<button>';
+            html += '<i class="fa fa-forward"></i>';
+            html += '</button>';
+
+            html += '<button>';
+            html += '<i class="fa fa-fast-forward"></i>';
+            html += '</button>';
+
+            html += '<div>';
             // Размер
-            tfooter += '<select>';
-
             var avialablePageSizes = [5, 10, 20, 50, 100];
 
+            html += '<select>';
             $.each(avialablePageSizes,
                 (i, e) => {
-                    tfooter += '<option ' + (e === x.Size ? 'selected' : '')+ '>';
-                    tfooter += e;
-                    tfooter += '</option>';
+                    html += '<option ' + (e === x.Size ? 'selected' : '') + '>';
+                    html += e;
+                    html += '</option>';
                 });
 
-            tfooter += '</select>';
-            //
+            html += '</select>';
+            html += '</div>';
+            html += '</div>';
 
             // Количество
-            tfooter += '<span class="right">';
-            tfooter += 'Показано: ' + ((x.Page - 1) * x.Size + 1) + ' - ' +
+            html += '<span class="right">';
+            html += 'Показано: ' + ((x.Page - 1) * x.Size + 1) + ' - ' +
                 (x.Page * x.Size < x.TotalCount ? x.Page * x.Size : x.TotalCount) + ' из ' + x.TotalCount;
-            tfooter += '</span>';
-            //
-
-
-
-            tfooter += '</td>';
-            tfooter += '</tr>';
-            tfooter += '</tfoot>';
-
+            html += '</span>';
             
-            var d = x;
+            html += '</td>';
+            html += '</tr>';
+            html += '</tfoot>';
 
-            table.append(tfooter);
+            table.append(html);
+
+            // Initialize material select
+            $('select').material_select();
+
+            // Initialize material datepicker
+            $('.datepicker').pickadate();
 
         }).fail(e => alert(e));
 }
@@ -249,7 +314,7 @@ $(document).ready(() => {
             key: true,
             type: 'int',
             autocompleteUrl: '',
-            width: 60,
+            width: 50,
             align: 'left',
             hidden: false,
             search: true,
@@ -287,7 +352,7 @@ $(document).ready(() => {
             key: false,
             type: 'string',
             autocompleteUrl: '',
-            width: 100,
+            width: 80,
             align: 'left',
             hidden: false,
             search: true,
@@ -300,7 +365,7 @@ $(document).ready(() => {
             key: false,
             type: 'string',
             autocompleteUrl: '',
-            width: 150,
+            width: 10,
             align: 'left',
             hidden: false,
             search: true,
@@ -313,7 +378,7 @@ $(document).ready(() => {
             key: false,
             type: 'bool',
             autocompleteUrl: '',
-            width: 90,
+            width: 120,
             align: 'center',
             hidden: false,
             search: true,
@@ -326,7 +391,7 @@ $(document).ready(() => {
             key: false,
             type: 'bool',
             autocompleteUrl: '',
-            width: 90,
+            width: 120,
             align: 'center',
             hidden: false,
             search: true,
@@ -339,7 +404,7 @@ $(document).ready(() => {
             key: false,
             type: 'datetime',
             autocompleteUrl: '',
-            width: 110,
+            width: 150,
             align: 'center',
             hidden: false,
             search: true,
@@ -352,7 +417,7 @@ $(document).ready(() => {
             key: false,
             type: 'datetime',
             autocompleteUrl: '',
-            width: 110,
+            width: 150,
             align: 'center',
             hidden: false,
             search: true,
