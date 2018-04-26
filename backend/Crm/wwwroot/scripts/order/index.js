803d49649bd58d1c22d14c65c98e986a1bcbed19
Usman Ajupov <au073@mail.ru>
2018-04-27 00:29:48 +0300

    фыв

--- backend/Crm/wwwroot/scripts/order/index.js
+++ backend/Crm/wwwroot/scripts/order/index.js
@@ -20,27 +20,23 @@ function initOrdersTable() {
             { Name: 'ClientId', Type: 'number', Hidden: true, Editable: true },
             {
                 Name: 'ClientName', Label: 'Клиент', Type: 'autocomplete', Editable: true, Width: 120,
-                AutocompleteUrl: '/Clients/GetForAutocompleteByNamePattern', AutocompleteHidden: 'ClientId',
+                AutocompleteUrl: '/Clients/GetAutocomplete', AutocompleteHidden: 'ClientId',
                 Formatter: clientLinkFormatter, Unformatter: linkUnFormatter
             },
             {
                 Name: 'SourceId', Label: 'Источник', Type: 'select', Editable: true, Sortable: false, Width: 120,
-                SelectValues: getSelectValues('/OrderSources/GetForSelect')
+                SelectValues: getSelectValues('/OrderSources/GetSelect')
             },
             {
                 Name: 'StatusId', Label: 'Статус', Type: 'select', Editable: true, Sortable: false, Width: 120,
-                SelectValues: getSelectValues('/OrderStatuses/GetForSelect')
+                SelectValues: getSelectValues('/OrderStatuses/GetSelect')
             },
             { Name: 'TotalSum', Label: 'Сумма', Type: 'money', Width: 80, Editable: true },
             { Name: 'DiscountSum', Label: 'Скидка', Type: 'money', Width: 80, Editable: true },
             { Name: 'IsDeleted', Label: 'Удалён', Type: 'checkbox', Width: 50, Editable: true, Sortable: false },
             { Name: 'CreateDate', Label: 'Дата создания', Type: 'datetime', Width: 110 },
             { Name: 'ModifyDate', Label: 'Дата изменения', Type: 'datetime', Width: 110 }
-        ],
-        OnSelectRow: id => {
-            $('#orders-items-table').jqGrid('setGridParam', { postData: { OrderId: id } }).trigger('reloadGrid');
-            $('#orders-attributes-table').jqGrid('setGridParam', { postData: { OrderId: id } }).trigger('reloadGrid');
-        }
+        ]
     });
 
     createTable({
@@ -109,22 +105,19 @@ function initAttributesTable() {
         Title: 'Атрибуты',
         Element: '#attributes-table',
         Pager: '#attributes-table-pagination',
-        IsViewable: true,
+        IsFilterable: true,
         IsEditable: true,
         IsCreatable: true,
         IsDeletable: true,
-        IsFilterable: true,
+        SortingColumn: 'Name',
         DataUrl: '/OrderAttributes/GetList',
         CreateUrl: '/OrderAttributes/Create',
         UpdateUrl: '/OrderAttributes/Update',
         DeleteUrl: '/OrderAttributes/Delete',
         Columns: [
-            { Name: 'Id', Label: '№', Type: 'number', Width: 60 },
-            { Name: 'Name', Label: 'Название', Type: 'text', Width: 120, Editable: true, MaxLength: 256 },
-            { Name: 'Description', Label: 'Описание', Type: 'text', Width: 120, Editable: true, MaxLength: 256 },
-            { Name: 'IsDeleted', Label: 'Удалён', Type: 'checkbox', Width: 50, Editable: true, Sortable: false },
-            { Name: 'CreateDate', Label: 'Дата создания', Type: 'datetime', Width: 110 },
-            { Name: 'ModifyDate', Label: 'Дата изменения', Type: 'datetime', Width: 110 }
+            { Name: 'Id', Label: '№', Type: 'number', Hidden: true },
+            { Name: 'Key', Label: 'Ключ', Type: 'text', Width: 150, Editable: true, MaxLength: 256 },
+            { Name: 'Name', Label: 'Название', Type: 'text', Width: 300, Editable: true, MaxLength: 256 }
         ]
     });
 }
@@ -134,20 +127,16 @@ function initOthersTable() {
         Title: 'Источник заказа',
         Element: '#orders-sources-table',
         Pager: '#orders-sources-table-pagination',
-        IsViewable: true,
         IsEditable: true,
         IsCreatable: true,
         IsDeletable: true,
-        IsFilterable: true,
         DataUrl: '/OrderSources/GetList',
         CreateUrl: '/OrderSources/Create',
         UpdateUrl: '/OrderSources/Update',
         DeleteUrl: '/OrderSources/Delete',
         Columns: [
-            { Name: 'Id', Label: '№', Type: 'number', Width: 60 },
-            { Name: 'Name', Label: 'Название', Type: 'text', Width: 120, Editable: true, MaxLength: 256 },
-            { Name: 'CreateDate', Label: 'Дата создания', Type: 'datetime', Width: 110 },
-            { Name: 'ModifyDate', Label: 'Дата изменения', Type: 'datetime', Width: 110 }
+            { Name: 'Id', Label: '№', Type: 'number', Hidden: true },
+            { Name: 'Name', Label: 'Название', Type: 'text', Width: 300, Editable: true, MaxLength: 256 }
         ]
     });
 
@@ -155,20 +144,16 @@ function initOthersTable() {
         Title: 'Статус заказа',
         Element: '#orders-statuses-table',
         Pager: '#orders-statuses-table-pagination',
-        IsViewable: true,
         IsEditable: true,
         IsCreatable: true,
         IsDeletable: true,
-        IsFilterable: true,
         DataUrl: '/OrderStatuses/GetList',
         CreateUrl: '/OrderStatuses/Create',
         UpdateUrl: '/OrderStatuses/Update',
         DeleteUrl: '/OrderStatuses/Delete',
         Columns: [
-            { Name: 'Id', Label: '№', Type: 'number', Width: 60 },
-            { Name: 'Name', Label: 'Название', Type: 'text', Width: 120, Editable: true, MaxLength: 256 },
-            { Name: 'CreateDate', Label: 'Дата создания', Type: 'datetime', Width: 110 },
-            { Name: 'ModifyDate', Label: 'Дата изменения', Type: 'datetime', Width: 110 }
+            { Name: 'Id', Label: '№', Type: 'number', Hidden: true },
+            { Name: 'Name', Label: 'Название', Type: 'text', Width: 300, Editable: true, MaxLength: 256 }
         ]
     });
 }
